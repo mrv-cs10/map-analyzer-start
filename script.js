e0903f92011348217eacd79293eeb1f09035c13a
mrveldkamp <colin.veldkamp@epsb.ca>
2022-09-26 11:08:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// MAP ANALYZER (ZELDA)   12 cols 8 rows
-'use strict'; // Use strict mode
+// MAP ANALYZER  12 cols 8 rows
 
 // Global Variables
-let level = 'level1';
+let level = "level1";
 let mapData;
 let mapEls = [];
-fetch('maps.json')
+
+// Load Map Data from JSON File
+fetch("maps.json")
   .then((res) => res.json())
   .then((data) => {
     mapData = data;
     initMapHTML();
+    drawMap("level1");
   });
 
 // Helper Functions
 function initMapHTML() {
-  let containerEl = document.getElementById('container');
+  let containerEl = document.getElementById("container");
 
   // First Row (Column Numbers)
-  containerEl.appendChild(document.createElement('div')); // Empty Top Left Corner
+  containerEl.appendChild(document.createElement("div")); // Empty Top Left Corner
   for (let col = 0; col < 12; col++) {
-    let divEl = document.createElement('div');
+    let divEl = document.createElement("div");
     divEl.innerHTML = col;
     containerEl.appendChild(divEl);
   }
@@ -27,26 +29,23 @@ function initMapHTML() {
   // Remaining Rows (Row Number and Tiles)
   for (let row = 0; row < 8; row++) {
     // Row Number
-    let divEl = document.createElement('div');
+    let divEl = document.createElement("div");
     divEl.innerHTML = row;
     containerEl.appendChild(divEl);
     // Row Tiles
     let tempRowEls = [];
     for (let col = 0; col < 12; col++) {
-      let divEl = document.createElement('div');
+      let divEl = document.createElement("div");
       divEl.dataset.row = row;
       divEl.dataset.col = col;
-      divEl.className = 'tile';
-      divEl.addEventListener('mouseenter', mouseEnterHandler);
-      divEl.addEventListener('mouseleave', mouseLeaveHandler);
+      divEl.className = "tile";
+      divEl.addEventListener("mouseenter", mouseEnterHandler);
+      divEl.addEventListener("mouseleave", mouseLeaveHandler);
       containerEl.appendChild(divEl);
       tempRowEls.push(divEl);
     }
     mapEls.push(tempRowEls);
   }
-
-  // Draw Level 1 Map
-  drawMap('level1');
 }
 
 function drawMap(level) {
@@ -60,20 +59,20 @@ function drawMap(level) {
 
 // Event Listeners
 function mouseEnterHandler(e) {
-  e.target.classList.add('active');
+  e.target.classList.add("active");
   row = e.target.dataset.row;
   column = e.target.dataset.col;
-  document.getElementById('row').innerHTML = row;
-  document.getElementById('col').innerHTML = column;
+  document.getElementById("row").innerHTML = row;
+  document.getElementById("col").innerHTML = column;
 
   testTiles();
 }
 
 function mouseLeaveHandler(e) {
-  e.target.classList.remove('active');
+  e.target.classList.remove("active");
 }
 
-document.getElementById('level-select').addEventListener('input', levelSelect);
+document.getElementById("level-select").addEventListener("input", levelSelect);
 
 function levelSelect(e) {
   level = e.target.value;
@@ -81,33 +80,33 @@ function levelSelect(e) {
 }
 
 function testTiles() {
-  if (level === 'level1') {
+  if (level === "level1") {
     level1Solution();
-  } else if (level === 'level2') {
+  } else if (level === "level2") {
     level2Solution();
-  } else if (level === 'level3') {
+  } else if (level === "level3") {
     level3Solution();
-  } else if (level === 'level4') {
+  } else if (level === "level4") {
     level4Solution();
-  } else if (level === 'level5') {
+  } else if (level === "level5") {
     level5Solution();
-  } else if (level === 'level6') {
+  } else if (level === "level6") {
     level6Solution();
-  } else if (level === 'level7') {
+  } else if (level === "level7") {
     level7Solution();
-  } else if (level === 'level8') {
+  } else if (level === "level8") {
     level8Solution();
-  } else if (level === 'level9') {
+  } else if (level === "level9") {
     level9Solution();
-  } else if (level === 'level10') {
+  } else if (level === "level10") {
     level10Solution();
-  } else if (level === 'level11') {
+  } else if (level === "level11") {
     level11Solution();
-  } else if (level === 'level12') {
+  } else if (level === "level12") {
     level12Solution();
-  } else if (level === 'level13') {
+  } else if (level === "level13") {
     level13Solution();
-  } else if (level == 'level14') {
+  } else if (level == "level14") {
     level14Solution();
   }
 }
